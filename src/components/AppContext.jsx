f553f676a318d855d50da31d8354d0a5fbc50625
Udhaykrishn <krishnauday320@gmail.com>
2023-10-24 14:00:01 +0530

    Added AppContext

---
+++ src/components/AppContext.jsx
@@ -0,0 +1,18 @@
+// AppContext.js
+import { createContext, useContext, useState } from 'react';
+
+const AppContext = createContext();
+
+export function AppProvider({ children }) {
+  const [sharedData, setSharedData] = useState([]);
+
+  return (
+    <AppContext.Provider value={{ sharedData, setSharedData }}>
+      {children}
+    </AppContext.Provider>
+  );
+}
+
+export function useAppContext() {
+  return useContext(AppContext);
+}
