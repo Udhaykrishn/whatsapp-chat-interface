5a0c350ef6d3c1ed7e50b24887ee3787e1407ab2
Udhaykrishn <krishnauday320@gmail.com>
2023-10-29 00:31:24 +0530

    first commit

--- src/components/Body.jsx
+++ src/components/Body.jsx
@@ -1,15 +1,21 @@
-import { useAppContext } from "./AppContext";
 
+import { useAppContext } from "./AppContext";
+const formatTimestamp = (timestamp) => {
+  const options = { hour: 'numeric', minute: 'numeric' };
+  return timestamp.toLocaleTimeString(undefined, options);
+};
 const Body = () => {
   const { sharedData } = useAppContext();
   
   return (
-    <div className=" scroll-m-0 h-[894px] translate-y-20 overflow-x-scroll  w-full   bg-slate-600">
-      <ul>
-        {sharedData.map((data,index)=>(
-            <li className="text-3xl text-white text-center" key={index}>{data}</li>
+    <div className="scroll-m-0 h-[894px] translate-y-20 overflow-x-scroll w-full"style={{ backgroundImage: 'url("../../public/ng.png")' }}>
+      <div className="w-full">
+        {sharedData.map((data, index) => (
+          <div key={index} className="bg-blue-200 rounded-lg p-4 w-40 h-13 ml-auto mb-4 flex items-center hover:animate-pulse">
+            <p className="text-blue-800 text-center">{data}</p>
+          </div>
         ))}
-      </ul>
+      </div>
     </div>
   );
 };
